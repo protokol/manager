40a36644f513549af152b8437a33fea5b2273607
lubejmatej <matej@protokol.com>
2020-06-10 09:56:29 +0200

    Initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('protokol-manager app is running!');
+    expect(page.getTitleText()).toEqual('Hello world protokol-manager');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('protokol-manager app is running!');
+    expect(compiled.textContent).toContain('Hello world protokol-manager');
   });
 });
