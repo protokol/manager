24d8777a056bc4ba6eda8fe612b5dba41bcc7134
lubejmatej <matej@protokol.com>
2020-07-08 11:19:09 +0200

    feat(wallets): add collections, asset to wallet details

--- src/app/dashboard/pages/assets/assets.module.ts
+++ src/app/dashboard/pages/assets/assets.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { AssetsRoutingModule } from './assets-routing.module';
 import { AssetsComponent } from './assets.component';
-import { NgxsModule } from '@ngxs/store';
 import { SharedModule } from '@shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NzGridModule, NzModalModule } from 'ng-zorro-antd';
-import { AssetsState } from '@app/dashboard/pages/assets/state/assets/assets.state';
 import { AssetViewModalComponent } from '@app/dashboard/pages/assets/components/asset-view-modal/asset-view-modal.component';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
 import { JsonSchemaFormModule } from '@ajsf/core';
@@ -14,7 +12,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   imports: [
     AssetsRoutingModule,
-    NgxsModule.forFeature([AssetsState]),
     CommonModule,
     SharedModule,
     NzModalModule,
