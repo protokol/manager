2319bf40f03f9733165c19505ca0d6069c09ba37
lubejmatej <matej@protokol.com>
2020-07-20 15:33:22 +0200

    feat(core-manager): add node manager page

---
+++ src/app/dashboard/pages/nodes/components/node-manager-settings-modal/node-manager-settings-modal.component.ts
@@ -0,0 +1,84 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { SetCoreManagerPort } from '@core/store/network/networks.actions';
+import { untilDestroyed } from '@core/until-destroyed';
+import { tap } from 'rxjs/operators';
+import { NodeManagerService } from '@core/services/node-manager.service';
+import { NzMessageService, NzModalRef } from 'ng-zorro-antd';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-node-manager-settings-modal',
+  templateUrl: './node-manager-settings-modal.component.html',
+  styleUrls: ['./node-manager-settings-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NodeManagerSettingsModalComponent implements OnInit, OnDestroy {
+  managerSettingsForm!: FormGroup;
+
+  isLoading$ = new BehaviorSubject(false);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store,
+    private nodeManagerService: NodeManagerService,
+    private nzModalRef: NzModalRef,
+    private router: Router,
+    private nzMessageService: NzMessageService
+  ) {
+    this.createForm();
+  }
+
+  ngOnInit(): void {}
+
+  private createForm() {
+    this.managerSettingsForm = this.formBuilder.group({
+      port: [
+        '',
+        [Validators.required, Validators.min(1), Validators.max(65535)],
+      ],
+    });
+  }
+
+  onManagerSettingsFormSubmit(event: MouseEvent) {
+    event.preventDefault();
+
+    if (this.isLoading$.getValue()) {
+      return;
+    }
+
+    const { port } = this.managerSettingsForm.value;
+
+    this.store.dispatch(new SetCoreManagerPort(port));
+
+    this.isLoading$.next(true);
+    this.nodeManagerService
+      .infoCoreVersion()
+      .pipe(
+        untilDestroyed(this),
+        tap(
+          () => {
+            this.router.navigate(['/dashboard/nodes/manager']);
+            this.nzMessageService.success('Core manager connected!');
+            this.nzModalRef.destroy();
+          },
+          () => {
+            this.nzMessageService.error(
+              `Core manager not available on port:"${port}"!`
+            );
+            this.isLoading$.next(false);
+          }
+        )
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {}
+}
--- src/app/dashboard/pages/nodes/nodes.module.ts
+++ src/app/dashboard/pages/nodes/nodes.module.ts
@@ -4,13 +4,21 @@ import { CommonModule } from '@angular/common';
 import { NodesRoutingModule } from '@app/dashboard/pages/nodes/nodes-routing.module';
 import { NodeDetailsComponent } from '@app/dashboard/pages/nodes/components/node-details/node-details.component';
 import {
+  NzButtonModule,
   NzDescriptionsModule,
   NzDividerModule,
+  NzFormModule,
   NzGridModule,
+  NzIconModule,
+  NzInputModule,
+  NzModalModule,
   NzSpinModule,
   NzTypographyModule,
 } from 'ng-zorro-antd';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
+import { NodeManagerSettingsModalComponent } from './components/node-manager-settings-modal/node-manager-settings-modal.component';
+import { NodeManagerDetailsComponent } from './components/node-manager-details/node-manager-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -23,9 +31,19 @@ import { NgJsonEditorModule } from 'ang-jsoneditor';
     NzDividerModule,
     NgJsonEditorModule,
     NzGridModule,
+    NzButtonModule,
+    NzIconModule,
+    NzFormModule,
+    NzInputModule,
+    ReactiveFormsModule,
+    NzModalModule,
   ],
   providers: [],
-  declarations: [NodeDetailsComponent],
+  declarations: [
+    NodeDetailsComponent,
+    NodeManagerDetailsComponent,
+    NodeManagerSettingsModalComponent,
+  ],
   exports: [],
 })
 export class NodesModule {}
