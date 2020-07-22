b5f8428265bb38972a5b7f856f533cf81341fb67
lubejmatej <matej@protokol.com>
2020-07-22 12:17:11 +0200

    refactor: core manager methods file

--- src/app/@core/interfaces/core-manager-methods.enum.ts
+++ src/app/@core/interfaces/core-manager-methods.enum.ts
@@ -1,5 +1,7 @@
 export enum CoreManagerMethods {
   infoCoreVersion = 'info.coreVersion',
+  infoCoreStatus = 'info.coreStatus',
+  infoNextForgingSlot = 'info.nextForgingSlot',
   infoBlockchainHeight = 'info.blockchainHeight',
   logArchived = 'log.archived',
   processList = 'process.list',
