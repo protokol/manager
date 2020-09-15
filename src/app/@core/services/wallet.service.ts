9d0ab4899d64a3c800fc5ed1064a6e16dc63f834
Matej Lubej <matej@protokol.com>
2020-09-15 17:32:07 +0200

    fix(registration): form submit button

--- src/app/@core/services/wallet.service.ts
+++ src/app/@core/services/wallet.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Logger } from '@core/services/logger.service';
 import { ElectronUtils } from '@core/utils/electron-utils';
-
-// If you import a module but never use any of the imported values other than as TypeScript types,
-// the resulting javascript file will look as if you never imported the module at all.
-import * as bip39Type from 'bip39';
 import { ArkCryptoService } from '@core/services/ark-crypto.service';
 import { Interfaces as ArkInterfaces } from '@arkecosystem/crypto';
 import { StoreUtilsService } from '@core/store/store-utils.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+// If you import a module but never use any of the imported values other than as TypeScript types,
+// the resulting javascript file will look as if you never imported the module at all.
+import * as bip39Type from 'bip39';
 
 export enum MnemonicGenerateLanguage {
   ENGLISH = 'english',
