b6f901557de651eec60dc0647e36f7c88c791061
Matej Lubej <matej@protokol.com>
2020-10-13 15:40:56 +0200

    feat(terminal): font size

--- src/app/dashboard/pages/nodes/interfaces/node.types.ts
+++ src/app/dashboard/pages/nodes/interfaces/node.types.ts
@@ -3,3 +3,23 @@ export enum NodeManagerLoginSettingsEnum {
   Token = 'token',
   Basic = 'basic',
 }
+
+export enum TerminalFontSize {
+  xxSmall = 9,
+  xSmall = 10,
+  small = 13,
+  medium = 16,
+  large = 18,
+  xLarge = 24,
+  xxLarge = 32
+}
+
+export const TerminalFontSizes = [
+  TerminalFontSize.xxSmall,
+  TerminalFontSize.xSmall,
+  TerminalFontSize.small,
+  TerminalFontSize.medium,
+  TerminalFontSize.large,
+  TerminalFontSize.xLarge,
+  TerminalFontSize.xxLarge
+];
