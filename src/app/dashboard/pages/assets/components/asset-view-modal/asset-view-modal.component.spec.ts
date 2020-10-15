6914af799e404b6288bafbc904e3ced2dc337bac
Matej Lubej <matej@protokol.com>
2020-10-15 13:10:57 +0200

    test: update asset, collection view modal tests

--- src/app/dashboard/pages/assets/components/asset-view-modal/asset-view-modal.component.spec.ts
+++ src/app/dashboard/pages/assets/components/asset-view-modal/asset-view-modal.component.spec.ts
@@ -1,18 +1,21 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { inject, TestBed, waitForAsync } from '@angular/core/testing';
 import { AssetViewModalComponent } from '@app/dashboard/pages/assets/components/asset-view-modal/asset-view-modal.component';
 import { SharedModule } from '@shared/shared.module';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
 import { JsonSchemaFormModule } from '@ajsf/core';
 import { AjsfWidgetLibraryModule } from '@app/ajsf-widget-library/ajsf-widget-library.module';
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AssetViewModalComponent', () => {
-  let component: AssetViewModalComponent;
-  let fixture: ComponentFixture<AssetViewModalComponent>;
+  let modalService: NzModalService;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         imports: [
+          NoopAnimationsModule,
+          NzModalModule,
           AjsfWidgetLibraryModule,
           SharedModule,
           NgJsonEditorModule,
@@ -23,13 +26,12 @@ describe('AssetViewModalComponent', () => {
     })
   );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AssetViewModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(inject([NzModalService], (m: NzModalService) => {
+    modalService = m;
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const modalRef = modalService.create({ nzContent: AssetViewModalComponent });
+    modalRef.close();
   });
 });
