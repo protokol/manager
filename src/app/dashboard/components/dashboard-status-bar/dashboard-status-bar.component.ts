7eaf84fecbef3ff446e03bec934b95e5579a1c4f
lubejmatej <54802172+lubejmatej@users.noreply.github.com>
2020-11-19 16:40:57 +0100

    feat: latest block height indicator (#185)
    * chore: clean npm install
    
    * feat: latest block height indicator

--- src/app/dashboard/components/dashboard-status-bar/dashboard-status-bar.component.ts
+++ src/app/dashboard/components/dashboard-status-bar/dashboard-status-bar.component.ts
@@ -1,32 +1,54 @@
 import {
   ChangeDetectionStrategy,
-  Component,
+  Component, OnDestroy, OnInit
 } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { ProfilesState } from '@core/store/profiles/profiles.state';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, timer } from 'rxjs';
 import { ProfileWithId } from '@core/interfaces/profiles.types';
 import { NetworksState } from '@core/store/network/networks.state';
 import { Router } from '@angular/router';
 import { ProfileSelectModalComponent } from '@shared/components/profile-select-modal/profile-select-modal.component';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { environment } from '@env/environment';
+import { exhaustMap, tap } from 'rxjs/operators';
+import { BlockchainService } from '@core/services/blockchain.service';
+import { untilDestroyed } from '@core/until-destroyed';
+
 const { shell } = window.require('electron');
 
 @Component({
   selector: 'app-dashboard-status-bar',
   templateUrl: './dashboard-status-bar.component.html',
   styleUrls: ['./dashboard-status-bar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardStatusBarComponent {
+export class DashboardStatusBarComponent implements OnInit, OnDestroy {
   @Select(ProfilesState.getProfiles) profiles$: Observable<ProfileWithId[]>;
   @Select(NetworksState.getBaseUrl) getBaseUrl$: Observable<string>;
-  @Select(ProfilesState.getSelectedProfile) selectedProfile$: Observable<
-    ProfileWithId
-  >;
+  @Select(ProfilesState.getSelectedProfile) selectedProfile$: Observable<ProfileWithId>;
 
-  constructor(private router: Router, private nzModalService: NzModalService) {}
+  timer1$;
+  lastBlockHeight$ = new BehaviorSubject<number | null>(null);
+
+  constructor(private router: Router, private nzModalService: NzModalService, private blockchainService: BlockchainService) {
+  }
+
+  ngOnInit(): void {
+    this.timer1$ = timer(0, 8000)
+      .pipe(
+        untilDestroyed(this),
+        exhaustMap(() =>
+          this.blockchainService
+            .getLastBlock()
+            .pipe(
+              untilDestroyed(this),
+              tap(({ height }) => this.lastBlockHeight$.next(height))
+            )
+        )
+      )
+      .subscribe();
+  }
 
   onProfileSelect(event: MouseEvent, profileId: string) {
     event.preventDefault();
@@ -34,9 +56,9 @@ export class DashboardStatusBarComponent {
     this.nzModalService.create({
       nzContent: ProfileSelectModalComponent,
       nzComponentParams: {
-        profileId,
+        profileId
       },
-      nzFooter: null,
+      nzFooter: null
     });
   }
 
@@ -56,5 +78,9 @@ export class DashboardStatusBarComponent {
     const releasesUrl = environment.links
       .find(({ key }) => key === 'PROTOKOL-MANAGER-RELEASES')?.url;
     shell.openExternal(releasesUrl);
+  }
+
+  ngOnDestroy(): void {
+    this.timer1$.unsubscribe();
   }
 }
